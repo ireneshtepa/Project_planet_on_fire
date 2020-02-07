7672accb1de32e6d191b0e67e661f48e24fb0dac
Yashwinie <yashwinie.shivanand@gmail.com>
2020-02-07 14:55:44 -0500

    updates

---
+++ NASA_API/config.py
@@ -0,0 +1,5 @@
+# NASA API key
+
+api = "fTsy5sitCV2a9IA1XsFdrGdV98zOneSXIcpajthb"
+
+#You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example: https://api.nasa.gov/planetary/apod?api_key=fTsy5sitCV2a9IA1XsFdrGdV98zOneSXIcpajthb
