adf0650e218586b6dfe653d5006a8eecf3938889
Deyinka <deyinka23@gmail.com>
2020-02-13 11:36:52 -0500

    updates

--- Yatish/AppCode/Solved/static/js/logic.js
+++ Yatish/AppCode/Solved/static/js/logic.js
@@ -1,42 +1,28 @@
-var fireMarkers = [];
- 
- 
+// firemarkers plotting info from rested API
  url = "http://127.0.0.1:5000/api"
- jsonData = d3.json(url).then(function(data){
-  console.log(data); 
-
-  console.log(data[3])});
-//   for (i = 0; i < data.length; i++) {
-//      fireMarkers.push(data[i].latitude)
-//  };  
-
-//  for (i = 0; i < data.length; i++) {
-//  fireMarkers.push(L.marker(
-//   fireMarkers[i], {
-//   color: "red",
-//   fillColor: "red",
-//   radius: 500
-// }).bindPopup('fire'))
-
-console.log(fireMarkers);
- 
-
-//  for (var i = 0; i < cities.length; i++) {
-//    // loop through the cities array, create a new marker, push it to the cityMarkers array
-//    cityMarkers.push(
-//      L.marker(cities[i].location).bindPopup("<h1>" + cities[i].name + "</h1>")
-//    );
-//  }
- 
- // Add all the cityMarkers to a new layer group.
- // Now we can handle them as one group instead of referencing each individually
-
-
-
-
-
-
-//console.log(fireMarkers);
+ fire_cordinates = d3.json(url).then(function(data){
+  var coordinates = [];
+  var brightness = [];
+  console.log(data);
+   for (i = 0; i < data.length; i++) {
+    var loc_list = [data[i].latitude, data[i].longitude];
+    coordinates.push(loc_list)
+    var fire_temp = data[i].bright_ti4;
+    brightness.push(fire_temp)
+  };
+console.log(brightness)
+  fireMarkers = [];
+  for (var i = 0; i < coordinates.length; i++) {
+    // Setting the marker radius for the state by passing population into the markerSize function
+    fireMarkers.push(
+      L.circle(coordinates[i], {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "red",
+        fillColor: "red",
+        radius: 10000
+      })
+    )};
 
 // Define variables for base layers
 var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -53,7 +39,7 @@ var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?
   accessToken: API_KEY
 });
 
-// Create marker layer group
+// Create two separate layer group
 var fires = L.layerGroup(fireMarkers);
 
 // Create a baseMaps object
@@ -75,8 +61,17 @@ var myMap = L.map("map", {
   zoomControl: false
 });
 
-// Pass our map layers into our layer control
-// Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
-}).addTo(myMap);
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+ });
+
+
+// var continents = [
+//   {"Name": "Africa", "Location": [-0.025826, 23.432947], "Zoom": 4},
+//   {"Name": "Australia", "Location": [-25.985928, 133.881768], "Zoom": 5},
+//   {"Name": "Eurasia", "Location": [51.919220, 78.759420], "Zoom": 3},
+//   {"Name": "North America", "Location": [41.986402, -90.752039], "Zoom": 4},
+//   {"Name": "South America", "Location": [-18.808526, -57.954288], "Zoom": 4}
+// ];
